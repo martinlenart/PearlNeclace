d491f181bac5f30902182d52e8300fe2be98ef1f
Martin Lenart <martin_lenart@icloud.com>
2022-03-02 14:14:25 +0100

    Finalized Necklace in class form

--- PearlNeclace/Program.cs
+++ PearlNeclace/Program.cs
@@ -2,25 +2,26 @@
 using PearlNecklace;
 
 //Create a couple of Random pearls
+Console.WriteLine("Create a couple of Random pearls");
 Console.WriteLine(Pearl.Factory.CreateRandomPearl());
 Console.WriteLine(Pearl.Factory.CreateRandomPearl());
-Console.WriteLine();
 
-//Create a couple of Necklaces
+//Create a random Necklaces
+Console.WriteLine("\nCreate a random Necklaces");
 var necklace = Necklace.Factory.CreateRandomNecklace(35);
 Console.WriteLine(necklace);
 Console.WriteLine($"Nr of pearls: {necklace.Count()}");
 Console.WriteLine($"Nr of Freshwater pearls: {necklace.Count(PearlType.FreshWater)}");
 Console.WriteLine($"Nr of Saltwater pearls: {necklace.Count(PearlType.SaltWater)}");
 Console.WriteLine($"Price of the necklace: {necklace.Price}");
-Console.WriteLine();
 
-//Print the sorted Necklace
+//Sort the Necklace
+Console.WriteLine("\nSort the Necklace");
 necklace.Sort();
 Console.WriteLine(necklace);
-Console.WriteLine();
 
 //Find a pearl, test by using a kown pearl
+Console.WriteLine("\nFind a pearl, test by using a kown pearl");
 var findPearl = necklace[23];
 
 Console.WriteLine($"Looking for Pearl:\n{findPearl}");
@@ -34,5 +35,22 @@ else
 {
     Console.WriteLine($"Pearl found in position {idx}");
 }
+
+
+//Reference vs Value Pearl
+Console.WriteLine("\nReference vs Value Pearl");
+var p1 = Pearl.Factory.CreateRandomPearl();
+var p2 = Pearl.Factory.CreateRandomPearl();
+Console.WriteLine("Original pearls");
+Console.WriteLine($"p1: {p1}");
+Console.WriteLine($"p2: {p2}");
+p1 = p2;
+p1.Color = PearlColor.White;
+p1.Type = PearlType.SaltWater;
+p1.Shape = PearlShape.DropShaped;
+Console.WriteLine("\nAfter p1 = p2 assignment and change p2");
+Console.WriteLine($"p1: {p1}");
+Console.WriteLine($"p2: {p2}"); 
+
 
 
